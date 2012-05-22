dda92119098194454a9626ab525c2ce0e1de166f
Michael Storgaard <github@michaelstorgaard.com>
2012-05-22 15:50:00 +0200

    Added offset definition and example HTML

--- jcenter.jquery.js
+++ jcenter.jquery.js
@@ -1,4 +1,4 @@
-/*-------------------------------------------------------------------- 
+/*-----------------------------------------------------------------------
  * JQuery Plugin: "jCenter"
  * by:
    Michael Storgaard (js@michaelstorgaard.com)
@@ -9,20 +9,26 @@
  *
  * Description: Centers an element vertically in the browser window
  * Dependencies: jQuery library                               
- * Usage Example: $(element).jCenter();
+ * Usage Example:
+   $(element).jCenter();
+   Optional: Define top and left offset (to the center of the element) by
+   adding parameters like this: $(element).jCenter(10, 30);
+
  * Version: 1.0, 22.05.2012
  * Changelog:
  *  22.05.2012 initial Version 1.0
---------------------------------------------------------------------*/
+-----------------------------------------------------------------------*/
 (function($){
-    $.fn.jCenter = function(){
+    $.fn.jCenter = function(top, left){
         return this.each(function(){
+			top = (top) ? top : 50;
+			left = (left) ? left : 50;
             $(this).css({
                 'position': 'absolute',
-                'top': '50%',
-                'left': '50%',
-                'margin-top': ($(this).height()/-2)+'px',
-                'margin-left': ($(this).width()/-2)+'px'
+                'top': top+'%',
+                'left': left+'%',
+                'margin-top': ($(this).height()*-(top/100))+'px',
+                'margin-left': ($(this).width()*-(left/100))+'px'
             });
         });
     };
